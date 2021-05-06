535370dcfd90ef054343df91a6767377a18af012
Serge <foravitosells@gmail.com>
2021-05-06 22:12:19 +0300

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  
+  root :to => 'users#index'
+  resources :users
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'login' => "user_sessions#create"
+  post 'logout' => 'user_sessions#destroy', :as => :logout
+end
